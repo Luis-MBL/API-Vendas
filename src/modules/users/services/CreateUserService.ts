16c71f78162da27c096126a9edd46ff122313851
Luís Miguel Barros Lara <2001lmbl@gmail.com>
2021-06-28 21:05:48 -0300

    feat: add bcryptjs in create user

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import AppError from '@shared/errors/AppError';
+import { hash } from 'bcryptjs';
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import { UserRepository } from '../typeorm/repositories/UsersRepository';
@@ -18,7 +19,13 @@ class CreateUserService {
       throw new AppError('Email address already used');
     }
 
-    const user = usersRepository.create({ name, email, password });
+    const hashedPassword = await hash(password, 8);
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
     await usersRepository.save(user);
 
     return user;
